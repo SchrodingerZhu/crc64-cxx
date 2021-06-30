68fa751f76d56214232facd5caad2a98c3a8b84b
Schrodinger ZHU Yifan <i@zhuyi.fan>
2021-06-30 09:41:23 +0800

    adjust SIMD implementation
    Signed-off-by: Schrodinger ZHU Yifan <i@zhuyi.fan>

--- include/crc64/arch/aarch64.hpp
+++ include/crc64/arch/aarch64.hpp
@@ -33,39 +33,25 @@ namespace crc64 {
 
         SIMD operator^(const SIMD& that) const noexcept;
 
+        bool operator==(const SIMD &that) const noexcept;
+
+    private:
+
         detail::simd_t _inner {};
 
         explicit SIMD(detail::simd_t _inner) noexcept;
 
-        bool operator==(const SIMD &that) const noexcept;
-
-    };
-
-    namespace detail {
-
         using Poly64Pair = std::pair<poly64_t, poly64_t>;
 
-        static inline SIMD from_mul(poly64_t a, poly64_t b) {
-            auto mul = vmull_p64(a, b);
-            return SIMD(vreinterpretq_u8_p128(mul));
-        }
+        static SIMD from_mul(poly64_t a, poly64_t b);
 
-        static inline Poly64Pair into_poly64pair(SIMD x) {
-            auto y = vreinterpretq_p64_u8(x._inner);
-            return {vgetq_lane_p64(y, 0), vgetq_lane_p64(y, 1)};
-        }
+        Poly64Pair into_poly64pair() const noexcept;
 
-        static inline poly64_t high64(SIMD x) {
-            auto y = vreinterpretq_p64_u8(x._inner);
-            return vgetq_lane_p64(y, 1);
-        }
+        poly64_t high64() const noexcept;
 
-        static inline poly64_t low64(SIMD x) {
-            auto y = vreinterpretq_p64_u8(x._inner);
-            return vgetq_lane_p64(y, 0);
-        }
+        poly64_t low64() const noexcept;
 
-    }
+    };
 
     inline SIMD::SIMD(uint64_t high, uint64_t low) noexcept {
         _inner = vcombine_u8(vcreate_u8(low), vcreate_u8(high));
@@ -76,17 +62,17 @@ namespace crc64 {
     }
 
     inline SIMD SIMD::fold8(uint64_t coeff) const noexcept {
-        auto [x0, x1] = detail::into_poly64pair(*this);
-        auto h        = detail::from_mul(static_cast<poly64_t>(coeff), x0);
+        auto [x0, x1] = into_poly64pair();
+        auto h        = SIMD::from_mul(static_cast<poly64_t>(coeff), x0);
         auto l        = SIMD{0, static_cast<uint64_t>(x1)};
         return h.bitxor(l);
     }
 
     inline SIMD SIMD::fold16(SIMD coeff) const noexcept {
-//        auto [x0, x1] = detail::into_poly64pair(*this);
-//        auto [c0, c1] = detail::into_poly64pair(coeff);
-//        auto h = detail::from_mul(c0, x0);
-//        auto l = detail::from_mul(c1, x1);
+//        auto [x0, x1] = into_poly64pair();
+//        auto [c0, c1] = coeff.into_poly64pair();
+//        auto h = SIMD::from_mul(c0, x0);
+//        auto l = SIMD::from_mul(c1, x1);
         SIMD h(0, 0), l(0, 0);
         asm(
                 "pmull %0.1q, %2.1d, %3.1d\n"
@@ -98,10 +84,9 @@ namespace crc64 {
     }
 
     inline uint64_t SIMD::barrett(uint64_t poly, uint64_t mu) const noexcept {
-        auto t1 = detail::low64(
-                detail::from_mul(detail::low64(*this), static_cast<poly64_t>(mu)));
-        auto l  = detail::from_mul(t1, static_cast<poly64_t>(mu));
-        auto reduced = static_cast<uint64_t>(detail::high64(bitxor(l)));
+        auto t1 = SIMD::from_mul(low64(), static_cast<poly64_t>(mu)).low64();
+        auto l  = SIMD::from_mul(t1, static_cast<poly64_t>(mu));
+        auto reduced = static_cast<uint64_t>(bitxor(l).high64());
         return reduced ^ static_cast<uint64_t>(t1);
     }
 
@@ -122,6 +107,26 @@ namespace crc64 {
 
     inline SIMD SIMD::aligned(const void *address) noexcept {
         return SIMD { vld1q_u8(reinterpret_cast<const uint8_t *>(address)) };
+    }
+
+    inline SIMD SIMD::from_mul(poly64_t a, poly64_t b) noexcept {
+        auto mul = vmull_p64(a, b);
+        return SIMD(vreinterpretq_u8_p128(mul));
+    }
+
+    inline Poly64Pair SIMD::into_poly64pair() const noexcept {
+        auto y = vreinterpretq_p64_u8(_inner);
+        return {vgetq_lane_p64(y, 0), vgetq_lane_p64(y, 1)};
+    }
+
+    inline poly64_t SIMD::high64() const noexcept {
+        auto y = vreinterpretq_p64_u8(_inner);
+        return vgetq_lane_p64(y, 1);
+    }
+
+    inline poly64_t SIMD::low64() const noexcept {
+        auto y = vreinterpretq_p64_u8(_inner);
+        return vgetq_lane_p64(y, 0);
     }
 }
 
