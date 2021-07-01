63413424298149cc579e1474f9b63b11d827830e
Schrodinger ZHU Yifan <i@zhuyi.fan>
2021-07-01 10:36:23 +0800

    add var-length benchmark (#3)
    * feat(bench): add var-length support
    
    Signed-off-by: Schrodinger ZHU Yifan <i@zhuyi.fan>
    
    * feat(bench): add info about aarch64
    
    Signed-off-by: Schrodinger ZHU Yifan <i@zhuyi.fan>

--- benches/main.cpp
+++ benches/main.cpp
@@ -63,11 +63,53 @@ static void benchmark_crc64_long_table(benchmark::State &state) {
             4 * 1024 * 1024 * static_cast<int64_t>(state.iterations()));
 }
 
+static void benchmark_crc64_varlength_simd(benchmark::State &state) {
+    auto dev = std::random_device{};
+    auto eng = std::mt19937_64{dev()};
+    auto dist = std::uniform_int_distribution<char>{};
+    auto d = crc64::Digest{true};
+    std::vector<char> data(1024 * 1024);
+    for (auto &i : data) {
+        i = dist(eng);
+    }
+    for (auto _ : state) {
+        for (auto l = 1; l <  1024 * 1024; l <<= 1) {
+            d.write(data.data(), l);
+        }
+        benchmark::DoNotOptimize(d.checksum());
+    }
+
+    state.SetBytesProcessed(
+            2097151 * static_cast<int64_t>(state.iterations()));
+}
+
+
+static void benchmark_crc64_varlength_table(benchmark::State &state) {
+    auto dev = std::random_device{};
+    auto eng = std::mt19937_64{dev()};
+    auto dist = std::uniform_int_distribution<char>{};
+    auto d = crc64::Digest{false};
+    std::vector<char> data(1024 * 1024);
+    for (auto &i : data) {
+        i = dist(eng);
+    }
+    for (auto _ : state) {
+        for (auto l = 1; l <=  1024 * 1024; l <<= 1) {
+            d.write(data.data(), l);
+        }
+        benchmark::DoNotOptimize(d.checksum());
+    }
+
+    state.SetBytesProcessed(
+            2097151 * static_cast<int64_t>(state.iterations()));
+}
+
 // Register the function as a benchmark
 BENCHMARK(benchmark_crc64_fixed_simd);
 BENCHMARK(benchmark_crc64_long_simd);
 BENCHMARK(benchmark_crc64_fixed_table);
 BENCHMARK(benchmark_crc64_long_table);
-
+BENCHMARK(benchmark_crc64_varlength_simd);
+BENCHMARK(benchmark_crc64_varlength_table);
 
 BENCHMARK_MAIN();
