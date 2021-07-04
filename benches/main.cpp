164f02d9481a1f4e5cabf7766bcf1577d9fc15b9
Schrodinger ZHU Yifan <i@zhuyi.fan>
2021-07-04 15:18:33 +0800

    GiB Level test
    Signed-off-by: Schrodinger ZHU Yifan <i@zhuyi.fan>

--- benches/main.cpp
+++ benches/main.cpp
@@ -125,12 +125,56 @@ static void benchmark_crc64_varlength_table(benchmark::State& state)
   state.SetBytesProcessed(2097151 * static_cast<int64_t>(state.iterations()));
 }
 
+static void benchmark_crc64_gib_simd(benchmark::State& state)
+{
+  auto dev = std::random_device{};
+  auto eng = std::mt19937_64{dev()};
+  auto dist = std::uniform_int_distribution<char>{};
+  auto d = crc64::Digest{true};
+  std::vector<char> data(1024 * 1024 * 1024);
+  for (auto& i : data)
+  {
+    i = dist(eng);
+  }
+  for (auto _ : state)
+  {
+    UNUSED(_);
+    d.write(data.data(), data.size());
+    benchmark::DoNotOptimize(d.checksum());
+  }
+  state.SetBytesProcessed(
+    1024 * 1024 * 1024 * static_cast<int64_t>(state.iterations()));
+}
+
+static void benchmark_crc64_gib_table(benchmark::State& state)
+{
+  auto dev = std::random_device{};
+  auto eng = std::mt19937_64{dev()};
+  auto dist = std::uniform_int_distribution<char>{};
+  auto d = crc64::Digest{false};
+  std::vector<char> data(1024 * 1024 * 1024);
+  for (auto& i : data)
+  {
+    i = dist(eng);
+  }
+  for (auto _ : state)
+  {
+    UNUSED(_);
+    d.write(data.data(), data.size());
+    benchmark::DoNotOptimize(d.checksum());
+  }
+
+  state.SetBytesProcessed(
+    1024 * 1024 * 1024 * static_cast<int64_t>(state.iterations()));
+}
+
 // Register the function as a benchmark
 BENCHMARK(benchmark_crc64_fixed_simd);
 BENCHMARK(benchmark_crc64_long_simd);
+BENCHMARK(benchmark_crc64_varlength_simd);
+BENCHMARK(benchmark_crc64_gib_simd);
 BENCHMARK(benchmark_crc64_fixed_table);
 BENCHMARK(benchmark_crc64_long_table);
-BENCHMARK(benchmark_crc64_varlength_simd);
 BENCHMARK(benchmark_crc64_varlength_table);
-
+BENCHMARK(benchmark_crc64_gib_table);
 BENCHMARK_MAIN();
